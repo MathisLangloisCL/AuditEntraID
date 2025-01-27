07e5387c7ffad484a5ea42f9d32e85a6a384a1f7
MathisLangloisCL <mathis.langlois@cyberlift.fr>
2025-01-27 09:51:27 +0100

    Create AuditEntra.ps1

---
+++ AuditEntra.ps1
@@ -0,0 +1,32 @@
+# Clone le dépôt GitHub contenant le module BARK dans le répertoire courant
+git clone https://githubcom/BloodHoundAD/BARK
+
+# Importation du script BARKps1 situé dans le répertoire courant
+$Path = Join-Path -Path (Get-Location) -ChildPath "BARK/BARK.ps1"
+. $Path
+
+# Obtention d'un jeton d'accès Microsoft Graph en utilisant les identifiants d'une application (ID client, secret client et nom du tenant)
+# Ce jeton est nécessaire pour effectuer des requêtes via les API Microsoft Graph
+$MSGraphToken = (Get-MSGraphTokenWithClientCredentials `
+    -ClientID 'ClientID' `                 # Identifiant unique de l'application (remplacer 'ClientID' par la valeur réelle)
+    -ClientSecret 'ClientSecret' `         # Secret associé à l'application (remplacer 'ClientSecret' par la valeur réelle)
+    -TenantName 'ClientTenantName'         # Nom du tenant Azure (remplacer 'ClientTenantName' par la valeur réelle)
+).access_token
+
+# Obtention des identités d'applications critiques (Tier Zero Service Principals) via le module BARK
+# La commande "Get-EntraTierZeroServicePrincipals" retourne les principaux d'application avec des privilèges critiques
+Get-EntraTierZeroServicePrincipals -Token $MSGraphToken | %{
+    $tierZeroPrivilege = $_tiersZeroPrivilege # Stockage des privilèges Tier Zero associés à l'identité actuelle
+    
+    # Récupération des informations détaillées pour chaque Service Principal identifié
+    Get-EntraServicePrincipal `
+        -Token $MSGraphToken `
+        -ObjectID $_ServicePrincipalID |      
+        Select-Object id,                      
+            appDisplayName,                    
+            appOwnerOrganizationId,           
+            @{Name="InternalApplication"; Expression={ $_appOwnerOrganizationId -eq "TenantID" }}, # Identifiant unique du tenant Azure (remplacer 'TenantID' par la valeur réelle)
+            @{Name="TierZeroPrivilege"; Expression={ $tierZeroPrivilege }}
+} | Export-Csv -Path "Serviceprincipalscsv" -NoTypeInformation -Encoding UTF8
+# Exporte les informations collectées dans un fichier CSV appelé "Serviceprincipalscsv"
+
